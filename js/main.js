a0a8b282af40bc0ae75bebdcf964fb3987a66f8a
slemenik <ms4388@student.uni-lj.si>
2017-11-26 19:41:15 +0100

    Wait for keystrokes added

--- js/main.js
+++ js/main.js
@@ -16,25 +16,30 @@ var keys;
 var notes;
 var line1;
 
-var keyCode = ["a", "s", "d","f","g","h","j","k","l","1","2","3","4","5"];
-
+// var keyCode = {"a", "s", "d","f","g","h","j","k","l","1","2","3","4","5"};
+var keyCode = {a:65,s:83,d:68,f:70,g:71,h:72,j:74,k:75,l:76,č:0,"c":49,"v":50,"b":51,"n":52,"m":53};
+var keyboardKeys = ['a','s','d','f','g','h','j','k','l','č','c','v','b','n','m']; 
+var waitForKeys;
+var keyToPress;
 function create(){
-
 	game.stage.backgroundColor = '#124184';
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-		game.stage.disableVisibilityChange = true;
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	//game.stage.disableVisibilityChange = true;//ko greš iz okna se igra nadaljuje
 	keyData = game.cache.getImage('white-key');
-	//var keys = new Array();
 	position = 80;
 
 	keys = game.add.group();
     keys.enableBody = true;
-
+	
     var graphics=game.add.graphics(0,0);//if you have a static line
   	graphics.lineStyle(1, 0x000000, 1);
     for (var i = 0; i<15;i++ ){	
 	 	keyHeight = keyData.height;
 		var key = keys.create(position,game.world.height-keyHeight/2,'white-key');
+		keytext = game.add.text(key.x + 10,key.y + 10, keyboardKeys[i]);
+		key.inputEnabled = true;
+		key.events.onInputDown.add( listener, key );
+		key.name = "key" + i;
 		key.scale.setTo(0.5);
 
 		graphics.moveTo(position,0);//moving position of graphic if you draw mulitple lines
@@ -42,14 +47,8 @@ function create(){
 
 		position += keyData.width/2;
 		key.body.immovable = true;
-
-
-		//keys.push(key);
-
-		
   		
 	}
-
 	graphics.moveTo(position,0);//moving position of graphic if you draw mulitple lines
   	graphics.lineTo(position,game.world.height-keyHeight/2);
 
@@ -61,18 +60,57 @@ function create(){
 
 	//var line = new Phaser.Line(0, 0, 100, 100);
   	//var graphics=game.add.graphics(0,0);
-  	
+  	cursors = game.input.keyboard.createCursorKeys(); 
+  	this.game.input.keyboard.onPressCallback = function(e) {
+  		console.log(e);
 
+  		if (waitForKeys && e==keyToPress)   {
+  			game.paused = false;
+  			noteToKill.kill();
+
+  		}
+
+  		
+  		//console.log(keyCode);
+
+
+  	};
+
+  	waitForKeys = false;
+  	toggleMode = game.add.text(game.world.width - 120, 20, 'Čakaj: NE', { font: '24px Arial', fill: '#fff' });
+    toggleMode.inputEnabled = true;
+    toggleMode.events.onInputUp.add(function () {
+    	waitForKeys = !waitForKeys;
+    	toggleMode.text = waitForKeys ? 'Čakaj: DA' : 'Čakaj: NE';
+    });
+
+}
+
+function listener () {
+
+    //console.log("fdasfsa");
+    //toggleMode.text = "dsadasd";
 
 }
 
 var appearanceTime = 0;
-
+var noteToKill;
 function update (){
 	
 	//game.physics.arcade.collide(notes, keys);
 	game.physics.arcade.collide(notes, keys, 
-		function(notes, keys){ notes.kill(); }, null, this);
+		function(note, keys){ 
+			if (waitForKeys){
+				game.paused = true;
+				keyToPress = keyboardKeys[note.name.substring(4)];
+				noteToKill = note;
+				//console.log(keyCode[keyToPress]);
+
+			} else {
+				note.kill(); 
+			}
+			
+		}, null, this);
 	if (game.time.now > appearanceTime) {
             
             positionNum = game.rnd.integerInRange(0,14);
@@ -82,6 +120,7 @@ function update (){
 			note.body.velocity.y = 70;
 			note.body.collideWorldBounds = true;
 			note.tint = 0xff00ff;
+			note.name = "note" + positionNum;
 			note.events.onKilled.add(function(note){notes.remove(note)},this);
 			
 			
@@ -90,5 +129,5 @@ function update (){
     }
 
 
-    
+
 }
