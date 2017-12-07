26fdb966cf0f95a2d40d43d24d114c1666b50356
slemenik <ms4388@student.uni-lj.si>
2017-12-07 20:19:04 +0100

    Piano keys resemble actual piano

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-var game = new Phaser.Game(800, 800, Phaser.AUTO, 'canvas', 
+var game = new Phaser.Game(1300, 800, Phaser.AUTO, 'canvas', 
 	{ preload: preload, create: create, update: update });
 
 function preload(){
 	game.load.image('white-key', 'assets/img/white.png');
+	game.load.image('left-key', 'assets/img/left-small.png');
+	game.load.image('right-key', 'assets/img/right-small.png');
+	game.load.image('middle-key', 'assets/img/middle-small.png');
+	game.load.image('black-key', 'assets/img/black.png');
 	game.load.audio('audio', 'assets/aud/audio.mp3' );
 
 //  this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -26,35 +30,50 @@ var pianoKeys = ['C','C#', 'D', 'D#','E','F', 'F#','G', 'G#', 'A', 'A#', 'H'];
 var waitForKeys;
 var keyToPress;
 
+var octavesCount = 4;
+var minMaxOctave = {mario:[55, 84]};
+
 var points = 0;
 function create(){
 	game.stage.backgroundColor = '#124184';
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	//game.stage.disableVisibilityChange = true;//ko greš iz okna se igra nadaljuje
-	keyData = game.cache.getImage('white-key');
-	position = 80;
+	
+	position = 0;
 
 	keys = game.add.group();
     keys.enableBody = true;
 	
     var graphics=game.add.graphics(0,0);//if you have a static line
   	graphics.lineStyle(1, 0x000000, 1);
-    for (var i = 0; i<15;i++ ){	
-	 	keyHeight = keyData.height;
-		var key = keys.create(position,game.world.height-keyHeight/2,'white-key');
-		game.add.text(key.x + 10,key.y + 10, keyboardKeys[i]);
-		game.add.text(key.x + 5,key.y + 80, pianoKeys[i%12]);
-		key.inputEnabled = true;
-		key.events.onInputDown.add( listener, key );
-		key.name = "key" + i;
-		key.scale.setTo(0.5);
 
-		graphics.moveTo(position,0);//moving position of graphic if you draw mulitple lines
-  		graphics.lineTo(position,game.world.height-keyHeight/2);
+  	
+  	for (var j = 0; j<octavesCount;j++){
+  		keysOrder = [0,1,2,1,3,0,1,2,1,2,1,3];//0-left, 1-black, 2-middle, 3-right
+	    for (var i = 0; i<keysOrder.length;i++ ){
+	    	keyType = keysOrder[i];
+	    	keyName = (keyType==0 ? 'left-key' : 
+	    				keyType==1 ? 'black-key' : 
+	    				keyType==2 ? 'middle-key' : 'right-key');	
+		 	keyData = game.cache.getImage(keyName);
+		 	keyHeight = keyData.height;
+			var key = keys.create(position,game.world.height-keyHeight,keyName);
 
-		position += keyData.width/2;
-		key.body.immovable = true;
-  		
+			textColor = keyType == 1 ? '#fff' : '#000';
+			game.add.text(key.x + 5,key.y + 10, keyboardKeys[i], {fill: textColor});
+			game.add.text(key.x + 5,key.y + 80, pianoKeys[i%12], {fill: textColor, font: "bold 16px Arial"});
+			key.inputEnabled = true;
+			key.events.onInputDown.add( listener, key );
+			key.name = "key" + i;
+			//key.scale.setTo(0.5);
+
+			graphics.moveTo(position,0);//moving position of graphic if you draw mulitple lines
+	  		graphics.lineTo(position,game.world.height-keyHeight);
+
+			position += keyData.width;
+			key.body.immovable = true;
+	  		
+		}
 	}
 	graphics.moveTo(position,0);//moving position of graphic if you draw mulitple lines
   	graphics.lineTo(position,game.world.height-keyHeight/2);
@@ -89,7 +108,7 @@ function create(){
   		keyToColor = keyboardKeys.indexOf(e.key);
   		keys.children[keyToColor].tint = 0xf10f2f;
   	};
-
+  	
   	this.game.input.keyboard.onUpCallback = function(e){
   		keyToUncolor = keyboardKeys.indexOf(e.key);
   		keys.children[keyToUncolor].tint = 0xffffff;
@@ -135,7 +154,7 @@ function create(){
 		for (var j = 2; j<=3;j++){
 			midiNotes = midi.tracks[j].notes;
 			max = -1;
-			min = 89;//55-36 = 19 84-36=48
+			min = 89;//55-18 = 37 84-36=66
 		for (var i = 0; i<midiNotes.length;i++) {
 			
 			if (midiNotes[i].midi > max) max = midiNotes[i].midi;
