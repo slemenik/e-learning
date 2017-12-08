dae82904e98d8832b12c019503b625f9dd81c88e
slemenik <ms4388@student.uni-lj.si>
2017-12-08 03:31:09 +0100

    notes go down and up, pause disabled

--- js/main.js
+++ js/main.js
@@ -68,8 +68,8 @@ function create(){
 			textColor = keyType == 1 ? '#fff' : '#000';
 			game.add.text(key.x + 5,key.y + 10, keyboardKeys[index], {fill: textColor});
 			game.add.text(key.x + 5,key.y + 80, pianoKeys[i%12], {fill: textColor, font: "bold 16px Arial"});
-			game.add.text(key.x + 5,key.y + 120, 
-				getOctaveNumber(minMaxOctave[song][0]-(lowerOctaveFor*8))+ index, {font: "bold 16px Arial"});
+			// game.add.text(key.x + 5,key.y + 120, 
+			// 	getOctaveNumber(minMaxOctave[song][0]-(lowerOctaveFor*8))+ index, {font: "bold 16px Arial"});
 			sizeMidiMap[(getOctaveNumber(minMaxOctave[song][0]-(lowerOctaveFor*8))+ index)] = keyName;
 
 			key.inputEnabled = true;
@@ -100,7 +100,7 @@ function create(){
   	//var graphics=game.add.graphics(0,0);
   	cursors = game.input.keyboard.createCursorKeys(); 
   	this.game.input.keyboard.onPressCallback = function(e) {
-  		//console.log(e);
+  		console.log("0"+e);
 
   		if (waitForKeys && e==keyToPress)   {
   			game.paused = false;
@@ -112,31 +112,47 @@ function create(){
   		//28 je MIDDLE C
   		keyNumber = keyboardKeys.indexOf(e);
 
-  		console.log(getOctaveNumber(minMaxOctave[song][0]-(lowerOctaveFor*8)) + keyNumber);
+  		//console.log(getOctaveNumber(minMaxOctave[song][0]-(lowerOctaveFor*8)) + keyNumber);
   		audio.play('Tone' + (keyNumber + getOctaveNumber(minMaxOctave[song][0]-(lowerOctaveFor*8))));
 
   	};
 
   	this.game.input.keyboard.onDownCallback = function(e){
-  		//console.log(e);
+  		
   		keyToColor = keyboardKeys.indexOf(e.key);
   		keys.children[keyToColor].tint = 0xf10f2f;
+  		audio.play('Tone' + (keyNumber + getOctaveNumber(minMaxOctave[song][0]-(lowerOctaveFor*8))));
   	};
   	
   	this.game.input.keyboard.onUpCallback = function(e){
+  		console.log("2"+e)
+  		
   		keyToUncolor = keyboardKeys.indexOf(e.key);
   		keys.children[keyToUncolor].tint = 0xffffff;
   	};
 
   	waitForKeys = false;
-  	toggleMode = game.add.text(game.world.width - 120, 20, 'Čakaj: NE', { font: '24px Arial', fill: '#fff' });
+  	toggleMode = game.add.text(game.world.width - 120, 20, 'Od spodaj', { font: '24px Arial', fill: '#fff' });
     toggleMode.inputEnabled = true;
     toggleMode.events.onInputUp.add(function () {
-    	waitForKeys = !waitForKeys;
-    	toggleMode.text = waitForKeys ? 'Čakaj: DA' : 'Čakaj: NE';
+    	waitForKeys = true;
+    	console.log(game.time.events);
+    	// game.time.events = [];
+
+    	pomozna(-70);
+
     });
 
-    pointsText = game.add.text(game.world.width-120, 50, 'Točke: 0', { font: '24px Arial', fill: '#fff' });
+    toggleMode2 = game.add.text(game.world.width - 120, 40, 'Od zgoraj', { font: '24px Arial', fill: '#fff' });
+    toggleMode2.inputEnabled = true;
+    toggleMode2.events.onInputUp.add(function () {
+    	waitForKeys = true;
+
+    	pomozna(70);
+
+    });
+
+    //pointsText = game.add.text(game.world.width-120, 50, 'Točke: 0', { font: '24px Arial', fill: '#fff' });
 
     audio = game.add.audio('audio');
     audio.allowMultiple = true;
@@ -148,23 +164,17 @@ function create(){
     } 
 
 
-    //read MIDIq
-    MidiConvert.load("assets/aud/mario.mid", function(midi) {
-  		
+    //read MIDI
 
-		
-    	console.log(midi)
-		//midi = [1,2,3,4,5,6,7];
-		//audio.onStop.add(playNext, this, 0, midi.tracks[6].notes);
 
-		// for (var i = 0; i<midi.tracks.length;i++) {
-		// 	console.log(midi.tracks[i].name, midi.tracks[i].notes.length);
-		// 	track = midi.tracks[i];
-		// 	if (i == 2) {
-		// 		game.time.events.repeat(1000 * 0.3, 1000, playNext, this, track.notes);
-		// 	}
+    
 
-		// }
+  	
+
+}
+
+function pomozna(value){
+	MidiConvert.load("assets/aud/mario.mid", function(midi) {
 
 			max = -1;
 			min = 89;//55-18 = 37 84-36=66
@@ -180,33 +190,25 @@ function create(){
 					audio.play('Tone' + (midiNote.midi-(lowerOctaveFor*8)));
 
 					positionNum = positionArray[midiNote.midi - getOctaveNumber(minMaxOctave[song][0])] ;
-					console.log('Tone' + (midiNote.midi-(lowerOctaveFor*8)));
-					var note = notes.create(positionNum, 580, sizeMidiMap[
+					//console.log('Tone' + (midiNote.midi-(lowerOctaveFor*8)));
+					odKje = value == -70 ? 580 : 0;
+
+					var note = notes.create(positionNum, odKje, sizeMidiMap[
 						(midiNote.midi-(lowerOctaveFor*8))]);
 					note.scale.setTo(1, 0.1);
 					game.physics.arcade.enable(note);
-					note.body.velocity.y = -70;
+					note.body.velocity.y = value;
 					note.body.collideWorldBounds = false;
 					
 					note.tint = tint;
 					note.name = "note" + positionNum;
 					note.events.onKilled.add(function(note){notes.remove(note)},this);
-					
-					
-
-
-					//console.log(midiNote);
-					//if (audio._sound) audio._sound.playbackRate.value = 3;
+				
 				}, this, midiNotes[i], tint);
 			}
 		
-		}
-		console.log(min, max);
-		
+		}		
    	});
-
-  	
-
 }
 
 function getOctaveNumber(min){
@@ -221,27 +223,19 @@ var appearanceTime = 0;
 var noteToKill;
 function update (){
 	
+	
+		
+
 	//game.physics.arcade.collide(notes, keys);
 	game.physics.arcade.collide(notes, keys, 
-		function(note, keys){ 
-			if (waitForKeys){
-				game.paused = true;
-				keyToPress = keyboardKeys[note.name.substring(4)];
-				noteToKill = note;
-				//console.log(keyCode[keyToPress]);
-
-			} else {
-				//note.kill(); 
-			}
+		function(note, keys){
+		note.kill();  
+			
 			
 		}, null, this);
-	if (game.time.now > appearanceTime) {
-            
-            
+	
 
-    }
-
-    pointsText.text = 'Točke: ' + points;
+    //pointsText.text = 'Točke: ' + points;
 
 
 
